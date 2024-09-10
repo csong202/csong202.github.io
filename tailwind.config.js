eac362cbd4274ddce71cb9873cae76ff62698398
csong202 <csong097@uottawa.ca>
2024-09-10 14:36:55 -0400

    add colours to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {},
+		colors: {
+			background: "#0c0b20",
+			"background-nav": "#16153b",
+			teal: "#20bf9b",
+			"teal-light": "#6fc0ad",
+			pink: "#f043b3",
+			blue: "#2bc3f4",
+			"blue-dark": "#09316e",
+			purple: "#8e55e8",
+			"purple-dark": "#402669",
+		},
 	},
 	plugins: [],
 };
