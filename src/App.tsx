ff286d1110e2cc1b95cc9a455c10eb838feaf043
csong202 <csong097@uottawa.ca>
2024-09-10 14:24:18 -0400

    react-router-dom setup and folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: (
+			<div>
+				<h1>test test</h1>
+			</div>
+		),
+	},
+	{
+		path: "about",
+		element: <h1>about</h1>,
+	},
+	{
+		path: "work-experience",
+		element: <h1>work-experience</h1>,
+	},
+	{
+		path: "projects",
+		element: <h1>projects</h1>,
+	},
+	{
+		path: "contact",
+		element: <h1>contact</h1>,
+	},
+]);
+
 function App() {
-	return (
-		<div className="App">
-			<p className="text-xl">testing tailwind</p>
-		</div>
-	);
+	return <RouterProvider router={router} />;
 }
 
 export default App;
