8c1477a29cf068dabed9b107b47f4045c7fdef4f
csong202 <csong097@uottawa.ca>
2024-09-16 19:30:08 -0400

    add min height to PageWrapper

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -8,7 +8,7 @@ type PageWrapperProps = {
 const PageWrapper = (props: PageWrapperProps) => {
 	const { page } = props;
 	return (
-		<div className="w-dvw h-full p-0 bg-background text-purple">
+		<div className="w-dvw min-h-dvh p-0 bg-background text-purple">
 			<NavBar />
 			<div className="p-8 lg:p-12 xl:p-16">{page}</div>
 		</div>
