e92cc248c5429cf04ac46d9a9ffd77c21e14092b
csong202 <csong097@uottawa.ca>
2024-09-27 17:46:07 -0400

    remove DecoratedImage border

--- src/components/DecoratedImage.tsx
+++ src/components/DecoratedImage.tsx
@@ -9,17 +9,20 @@ const DecoratedImage = (props: DecoratedImageProps) => {
 		`border-${colour} border-solid border-2 rounded-md max-w-52 max-h-52 size-52`;
 
 	return (
-		<div className={getImgBorderClasses("pink")}>
-			<div
-				className={`${getImgBorderClasses("blue")} flex justify-center`}
-			>
-				<img
-					src={image}
-					alt="headshot"
-					className="max-w-48 max-h-48 size-48 self-center"
-				></img>
-			</div>
-		</div>
+		<>
+			{/* <div className={getImgBorderClasses("pink")}> */}
+			{/* TODO: Fix border colour bug. Only pink shows up, every other colour shows as purple */}
+			{/* <div
+				className={`${getImgBorderClasses("pink")} flex justify-center`}
+			> */}
+			<img
+				src={image}
+				alt="headshot"
+				className="max-w-48 max-h-48 size-48 self-center"
+			></img>
+			{/* </div> */}
+			{/* </div> */}
+		</>
 	);
 };
 
