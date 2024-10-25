4f8681ff14f9d92b94ed7cd0d0259e024f897d75
csong202 <csong097@uottawa.ca>
2024-10-25 15:04:55 -0400

    smaller home page text and padding

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ const Home = () => {
 	};
 
 	return (
-		<div className="bg-black p-3 text-white text-wrap">
+		<div className="bg-black p-2 text-white text-wrap text-sm leading-7">
 			<p>
 				{getTerminalPrompt("~")} {getColouredCmd("cd")}{" "}
 				dev/personal_website
