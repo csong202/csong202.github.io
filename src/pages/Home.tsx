952e35e904381b15179e940ecb7421fed5e27b11
csong202 <csong097@uottawa.ca>
2024-10-25 15:14:04 -0400

    max width full on home terminal prompt

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
 	const getTerminalPrompt = (dir: string) => (
-		<span>
+		<span className="max-w-full">
 			<span className="text-pink">csong202@Yoga7i:</span>
 			<span className="text-blue">
 				{/* using in line paragraph so it shows as one line on big screens and doesn't overflow on mobile */}
