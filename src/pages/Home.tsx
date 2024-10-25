6a3e6dae5196ad695dfb430654de73b8d6fe15da
csong202 <csong097@uottawa.ca>
2024-10-25 16:03:00 -0400

    home page file tree design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,48 +1,62 @@
+import { ReactComponent as FolderIcon } from "../icons/folder.svg";
+import { ReactComponent as HomeIcon } from "../icons/home.svg";
+import { ReactComponent as TextFileIcon } from "../icons/text-file.svg";
+
 const Home = () => {
-	const getTerminalPrompt = (dir: string) => (
-		<span className="max-w-full">
-			<span className="text-pink">csong202@Yoga7i:</span>
-			<span className="text-blue">
-				{/* using in line paragraph so it shows as one line on big screens and doesn't overflow on mobile */}
-				<p className="inline">{`${dir}$`}</p>
-			</span>
-		</span>
+	const iconClasses = "size-6 fill-blue hover:fill-pink inline pr-2";
+
+	const directory = (
+		indent: number,
+		icon: "home" | "folder" | "text",
+		name: string
+	) => (
+		<div
+			className={icon === "folder" || icon === "home" ? "text-teal" : ""}
+		>
+			<span>{"\u00A0\u00A0".repeat(indent)}</span>
+			{icon === "home" ? (
+				<HomeIcon className={iconClasses} />
+			) : icon === "folder" ? (
+				<FolderIcon className={iconClasses} />
+			) : (
+				<TextFileIcon className={iconClasses} />
+			)}
+			{name}
+		</div>
 	);
-	const getColouredCmd = (cmd: string) => {
-		return <span className="text-teal">{cmd}</span>;
-	};
 
 	return (
-		<div className="w-full bg-black p-2 text-white text-wrap text-sm leading-7">
-			<p>
-				{getTerminalPrompt("~")} {getColouredCmd("cd")}{" "}
-				dev/personal_website
-			</p>
-			<p>
-				{getTerminalPrompt("~/dev/personal_website")}{" "}
-				{getColouredCmd("cat")} intro.txt
-			</p>
-			<p>Hi, my name is Christina Song and this is my personal website</p>
-			<p>{getTerminalPrompt("~/dev/personal_website")}</p>
-			<p>
-				{getTerminalPrompt("~/dev/personal_website")}{" "}
-				{getColouredCmd("cat")} quick-summary.txt
-			</p>
-			<p>
-				{getTerminalPrompt("~/dev/personal_website")} I am a 4th year
-				computer science student at the University of Ottawa, and I am
-				seeking a full-time role starting in summer 2025. Currently
-				based in Ottawa and willing to relocate.
-			</p>
-			<p>{getTerminalPrompt("~/dev/personal_website")}</p>
-			<p>
-				{getTerminalPrompt("~/dev/personal_website")}{" "}
-				{getColouredCmd("cat")} next-steps.txt
-			</p>
-			<p>
-				{getTerminalPrompt("~/dev/personal_website")} Feel free to take
-				a look around!
-			</p>
+		<div className="w-full text-wrap break-words text-sm leading-7">
+			{directory(0, "home", "christina_song")}
+			{directory(1, "folder", "welcome_to_my_portfolio")}
+			{directory(2, "text", "feel_free_to_explore")}
+			{directory(2, "text", "my_projects_and_experience")}
+			{directory(1, "folder", "searching_for")}
+			{directory(2, "text", "full_time_software_dev_role")}
+			{directory(2, "text", "starting_summer_2025")}
+			{directory(2, "text", "willing_to_relocate")}
+			{directory(1, "folder", "quick_summary")}
+			{directory(2, "folder", "education")}
+			{directory(3, "text", "university_of_ottawa")}
+			{directory(3, "text", "bsc_honours_computer_science")}
+			{directory(3, "text", "fourth_year")}
+			{directory(2, "folder", "internships")}
+			{directory(3, "text", "intuit_turbotax")}
+			{directory(3, "text", "ciena")}
+			{directory(3, "text", "sap")}
+			{directory(3, "text", "aeponyx")}
+			{directory(3, "text", "tbs_canada")}
+			{directory(2, "folder", "skills")}
+			{directory(3, "folder", "languages")}
+			{directory(4, "text", "javascript_and_typescript")}
+			{directory(4, "text", "java")}
+			{directory(4, "text", "cpp_and_csharp")}
+			{directory(4, "text", "python")}
+			{directory(3, "folder", "frameworks")}
+			{directory(4, "text", "react")}
+			{directory(4, "text", "redux")}
+			{directory(4, "text", "spring_boot")}
+			{directory(4, "text", "aws_dynamodb")}
 		</div>
 	);
 };
