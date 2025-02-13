c00b7f5e1620dbe2a531aae297ea459bfbe09ad3
csong202 <csong097@uottawa.ca>
2025-02-13 15:29:22 -0500

    use HashRouter instead of BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { PageWrapper } from "./components";
 import { About, Contact, Home, Projects, WorkExperience } from "./pages";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
 	{
 		path: "/",
 		element: <PageWrapper page={<Home />}></PageWrapper>,
